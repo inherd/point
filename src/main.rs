eeb036b18fad2d64ec4f932ed3c68635bc1a5951
Phodal Huang <h@phodal.com>
2021-04-20 15:56:39 +0800

    refactor: move edit view to editor

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use druid::widget::prelude::*;
-use druid::widget::{Flex, Label, SizedBox, TextBox, WidgetExt};
-use druid::{AppLauncher, Color, Data, Lens, UnitPoint, WidgetId, WindowDesc};
+use druid::widget::{Flex, Label, WidgetExt};
+use druid::{AppLauncher, Color, Data, Lens, UnitPoint, WindowDesc};
+
+use print_ui::editor::EditView;
 
 pub mod line;
 pub mod menu;
@@ -24,91 +26,6 @@ struct ProjectState {
 #[derive(Clone, Data, Lens)]
 struct Params {
     debug_layout: bool,
-}
-
-/// builds a child Flex widget from some paramaters.
-struct EditView {
-    inner: Box<dyn Widget<Workspace>>,
-}
-
-impl EditView {
-    fn new() -> EditView {
-        EditView {
-            inner: SizedBox::empty().boxed(),
-        }
-    }
-
-    fn rebuild_inner(&mut self, data: &Workspace) {
-        let mut flex = Flex::row();
-
-        flex.add_child(
-            TextBox::multiline()
-                .with_placeholder("Sample text")
-                .with_text_color(Color::BLACK)
-                .fix_width(400.0)
-                .fix_height(600.0)
-                .lens(ProjectState::input_text)
-                .background(Color::WHITE),
-        );
-
-        let flex = flex
-            .expand_width()
-            .expand_height()
-            .background(line::hline())
-            .lens(Workspace::project);
-
-        if data.params.debug_layout {
-            self.inner = flex.debug_paint_layout().boxed()
-        } else {
-            self.inner = flex.boxed()
-        }
-    }
-}
-
-impl Widget<Workspace> for EditView {
-    fn event(&mut self, ctx: &mut EventCtx, event: &Event, data: &mut Workspace, env: &Env) {
-        self.inner.event(ctx, event, data, env)
-    }
-
-    fn lifecycle(
-        &mut self,
-        ctx: &mut LifeCycleCtx,
-        event: &LifeCycle,
-        data: &Workspace,
-        env: &Env,
-    ) {
-        if let LifeCycle::WidgetAdded = event {
-            self.rebuild_inner(data);
-        }
-        self.inner.lifecycle(ctx, event, data, env)
-    }
-
-    fn update(&mut self, ctx: &mut UpdateCtx, old_data: &Workspace, data: &Workspace, env: &Env) {
-        if !old_data.params.same(&data.params) {
-            self.rebuild_inner(data);
-            ctx.children_changed();
-        } else {
-            self.inner.update(ctx, old_data, data, env);
-        }
-    }
-
-    fn layout(
-        &mut self,
-        ctx: &mut LayoutCtx,
-        bc: &BoxConstraints,
-        data: &Workspace,
-        env: &Env,
-    ) -> Size {
-        self.inner.layout(ctx, bc, data, env)
-    }
-
-    fn paint(&mut self, ctx: &mut PaintCtx, data: &Workspace, env: &Env) {
-        self.inner.paint(ctx, data, env)
-    }
-
-    fn id(&self) -> Option<WidgetId> {
-        self.inner.id()
-    }
 }
 
 fn navigation_bar() -> impl Widget<Workspace> {
--- src/print_ui/editor.rs
+++ src/print_ui/editor.rs
@@ -1,3 +1,11 @@
+use druid::widget::{Flex, SizedBox, TextBox};
+use druid::{
+    BoxConstraints, Color, Data, Env, Event, EventCtx, LayoutCtx, LifeCycle, LifeCycleCtx,
+    PaintCtx, Size, UpdateCtx, Widget, WidgetExt, WidgetId,
+};
+
+use crate::{line, ProjectState, Workspace};
+
 pub struct Editor {
     pub input_value: String,
 }
@@ -12,4 +20,88 @@ impl Default for Editor {
 
 impl Editor {
     pub fn render(&mut self) {}
+}
+
+pub struct EditView {
+    inner: Box<dyn Widget<Workspace>>,
+}
+
+impl EditView {
+    pub fn new() -> EditView {
+        EditView {
+            inner: SizedBox::empty().boxed(),
+        }
+    }
+
+    fn rebuild_inner(&mut self, data: &Workspace) {
+        let mut flex = Flex::row();
+
+        flex.add_child(
+            TextBox::multiline()
+                .with_placeholder("Sample text")
+                .with_text_color(Color::BLACK)
+                .fix_width(400.0)
+                .fix_height(600.0)
+                .lens(ProjectState::input_text)
+                .background(Color::WHITE),
+        );
+
+        let flex = flex
+            .expand_width()
+            .expand_height()
+            .background(line::hline())
+            .lens(Workspace::project);
+
+        if data.params.debug_layout {
+            self.inner = flex.debug_paint_layout().boxed()
+        } else {
+            self.inner = flex.boxed()
+        }
+    }
+}
+
+impl Widget<Workspace> for EditView {
+    fn event(&mut self, ctx: &mut EventCtx, event: &Event, data: &mut Workspace, env: &Env) {
+        self.inner.event(ctx, event, data, env)
+    }
+
+    fn lifecycle(
+        &mut self,
+        ctx: &mut LifeCycleCtx,
+        event: &LifeCycle,
+        data: &Workspace,
+        env: &Env,
+    ) {
+        if let LifeCycle::WidgetAdded = event {
+            self.rebuild_inner(data);
+        }
+        self.inner.lifecycle(ctx, event, data, env)
+    }
+
+    fn update(&mut self, ctx: &mut UpdateCtx, old_data: &Workspace, data: &Workspace, env: &Env) {
+        if !old_data.params.same(&data.params) {
+            self.rebuild_inner(data);
+            ctx.children_changed();
+        } else {
+            self.inner.update(ctx, old_data, data, env);
+        }
+    }
+
+    fn layout(
+        &mut self,
+        ctx: &mut LayoutCtx,
+        bc: &BoxConstraints,
+        data: &Workspace,
+        env: &Env,
+    ) -> Size {
+        self.inner.layout(ctx, bc, data, env)
+    }
+
+    fn paint(&mut self, ctx: &mut PaintCtx, data: &Workspace, env: &Env) {
+        self.inner.paint(ctx, data, env)
+    }
+
+    fn id(&self) -> Option<WidgetId> {
+        self.inner.id()
+    }
 }
