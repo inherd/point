dd25f11c7b7cbd7808e48f6c3cace39c796c0278
Phodal Huang <h@phodal.com>
2021-04-21 20:00:47 +0800

    refactor: make open file simple

--- src/delegate.rs
+++ src/delegate.rs
@@ -22,32 +22,21 @@ impl AppDelegate<AppState> for Delegate {
                 ctx.submit_command(print_command::OPEN);
                 return Handled::Yes;
             }
-            return match infer::get_from_path(info.path()) {
-                Ok(typ) => match typ {
-                    None => {
-                        log::info!("unknown type: {:?}", info);
-                        Handled::No
-                    }
-                    Some(file_type) => {
-                        log::info!("file type: {:?}", file_type);
 
-                        match info.path().parent() {
-                            None => {}
-                            Some(parent) => {
-                                data.set_dir(Some(parent.to_owned()));
-                            }
-                        }
-
-                        data.set_file(info.path().to_owned());
-                        ctx.submit_command(print_command::OPEN);
-                        Handled::Yes
+            if let Ok(typ) = infer::get_from_path(info.path()) {
+                if let Some(_file_type) = typ {
+                    if let Some(parent) = info.path().parent() {
+                        data.set_dir(Some(parent.to_owned()));
                     }
-                },
-                Err(err) => {
-                    log::info!("error file type: {:?}", err);
-                    Handled::No
+
+                    data.set_file(info.path().to_owned());
+                    ctx.submit_command(print_command::OPEN);
+                    return Handled::Yes;
                 }
             };
+
+            log::info!("under type: {:?}", info);
+            return Handled::No;
         }
 
         Handled::No
