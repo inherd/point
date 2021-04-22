c85e3345122a1b094a3b1ccabfb0329cae78e4da
Phodal Huang <h@phodal.com>
2021-04-22 23:36:02 +0800

    feat: make create diretory

---
+++ src/support/directory.rs
@@ -0,0 +1,43 @@
+extern crate dirs;
+
+use crate::app_state::AppState;
+use serde_json::Error;
+use std::fs;
+use std::fs::OpenOptions;
+use std::io::Write;
+use std::path::PathBuf;
+
+pub fn save_config(state: &AppState) {
+    let result = serde_json::to_string(state);
+    match result {
+        Ok(str) => {
+            let path = config_path().expect("lost home issue");
+            let mut file = OpenOptions::new()
+                .read(true)
+                .write(true)
+                .create(true)
+                .open(&path)
+                .expect("unable to open file");
+
+            let result = file.write_all(str.as_bytes());
+
+            match result {
+                Ok(_) => log::info!("save file: {:?}", buf),
+                Err(e) => log::info!("failed to write data: {}", { e }),
+            }
+        }
+        Err(err) => {
+            log::info!("serialize config error: {:?}", err);
+        }
+    }
+}
+
+fn config_path() -> Option<PathBuf> {
+    let home = dirs::home_dir()?;
+    let base = home.join(".print");
+    if !&base.exists() {
+        let _ = fs::create_dir_all(&base);
+    }
+    let config_path = base.join("print.yaml");
+    Some(config_path)
+}
